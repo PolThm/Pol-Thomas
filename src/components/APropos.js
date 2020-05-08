04fd7f3213603928311fd387b41be50c865a115d
PolThm <pol.thomas56@gmail.com>
2020-05-08 15:04:36 +0200

    Petites retouches sur le design du Header

--- src/components/APropos.js
+++ src/components/APropos.js
@@ -13,19 +13,14 @@ class APropos extends Component {
         <div className={"a-propos"}>
           <div className={"a-propos-content"}>
             <div className={"texts-a-propos"}>
-              <ScrollAnimation
-                animateIn="fadeIn"
-                animateOnce="true"
-              >
+              <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
                 <h2 className={"a-propos-title"}>À PROPOS</h2>
               </ScrollAnimation>
-              <h3>Mon histoire</h3>
+              {/*<h3>Mon histoire</h3>*/}
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor arcu, bibendum ut leo ac, ultrices
                 interdum sem. Vestibulum sed faucibus risus. Suspendisse accumsan, diam id ultrices consectetur, ligula
                 ipsum pretium libero, quis vulputate purus tellus et urna. Nam et mauris ut orci ullamcorper cursus sed
-                ac ante. Nunc rutrum mauris quis volutpat hendrerit. Donec tempus metus sem, dignissim iaculis ligula
-                sollicitudin vel. Integer mattis nunc porta aliquet eleifend. Proin tellus dui, congue eu ante dapibus,
-                auctor consectetur odio.</p>
+                ac ante.</p>
             </div>
             <div className={"picture-a-propos"}>
               <Image className={"picture-a-propos"} src={polProfil} roundedCircle  />
@@ -33,7 +28,7 @@ class APropos extends Component {
           </div>
 
           <section className={'contactSection'}>
-            <ScrollAnimation animateIn="fadeIn" animateOnce="true">
+            <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
               <Button variant="secondary" className="buttonContact" onClick={this.showModalContact}>CONTACT</Button>
             </ScrollAnimation>
             {/*<ModalContact*/}
