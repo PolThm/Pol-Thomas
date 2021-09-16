3405e152cb57ccdb5c8f27d7b271b815c56c22de
pol.thomas <pol.thomas@niji.fr>
2021-09-16 20:27:35 +0200

    Fix accessibility on "Realisation"

--- src/components/Realisation.js
+++ src/components/Realisation.js
@@ -5,11 +5,13 @@ class Realisation extends React.Component {
   render() {
     return(
       <Figure onClick={() => window.open(this.props.link, '_blank')}>
-        <p className={"text-realisation"}>{this.props.description}</p>
-        <Figure.Image
-          src={this.props.picture}
-          alt={this.props.description}
-        />
+        <a target="_blank" rel="noopener noreferrer" href={this.props.link}>
+          <p className={"text-realisation"}>{this.props.description}</p>
+          <Figure.Image
+            src={this.props.picture}
+            alt={this.props.description}
+          />
+        </a>
       </Figure>
     )
   }
