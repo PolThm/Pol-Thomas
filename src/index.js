4297cb7c43d0338c941620ca10252cef3df6c5c1
pol.thomas <pol.thomas@niji.fr>
2022-10-03 12:43:21 +0200

    Redirection WIP

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import App from './App';
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
 
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path='*' element={<Navigate to='/' />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
