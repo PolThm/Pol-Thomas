d3bae46edc33cacd3096d0975567a20f66bc65e5
polthm <pol.thomas56@gmail.com>
2024-02-12 17:33:52 +0100

    fix: typo

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -19,7 +19,7 @@ class HeaderComponent extends Component {
           <h2 className="text-title">Software Engineer</h2>
           <div className="line-header"/>
           <h3 className="text-welcome">Welcome</h3>
-          <p className="text-introduction">Hello, I’m a French developer specializing in Front-End with TypeScript, expanding my skills to become Full-Stack. You could find below an overview of my work.</p>
+          <p className="text-introduction">Hello, I’m a French developer specialized in Front-End with TypeScript, expanding my skills to become Full-Stack. You could find below an overview of my work.</p>
         </Container>
         <div className={"downArrow"}>
           <a href={"#mes-realisations"}>
